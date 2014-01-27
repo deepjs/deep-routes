06f06a0c0f40d34765eb77b76fc74a3688043115
Gilles Coomans <gilles.coomans@gmail.com>
2014-01-27 09:32:43 +0100

    displace api examples to examples

--- example.js
+++ example.js
@@ -44,5 +44,90 @@ exampleMap.campaign.route("../campaign/12");
 
 
 
+/*
+
+
+view.refresh();     // refresh me. if not present in screen : not shown
+view.refresh(null, null, preloaded);     // refresh me with preloaded
+
+
+
+view.show();        // show me as this 
+view.refresh("./subs/[a,b,c]");     // refresh my subs
+view.refresh("../../!");      // refresh : ATTENTION = ma vue parente : pas le subs
+view.refresh("./subs/a", { id:12 });      // refresh un subs on lui collant
+
+// pour le controller info : ceci
+view.refresh({ id:"12" });  // refresh me with id = 12
+// est équivalent à
+view.refresh(".", "/info/12");  // refresh me with id = 12
+// et est encore equivalent à
+view.route("./info/12")    //from info change info but does only try its own route
+
+// from campaign : change campaign en testant aussi frêres
+view.route("../campaign/12/info/27")
+//from info change update
+view.route("../update/90")
+//from info change info but does only try its own route
+view.route("./info/12")           //         /campaign/12/info/12/update/45
+//from info change info but try brothers (in order of parent)
+view.route("../info/12")          //      /campaign/12/info/?query/1/34   /campaign/12/info/12
+// from any view 
+view.route("/campaign/45/update/90/profile/42")
+
+view.show()
+view.show({ id:34 })
+view.show("../[a,b,c]")
+
+view.hide()
+view.hide("../[a,b,c]")
+
+deep.refresh({
+    campaign:{
+      id:12,
+      info:{
+        id:24
+      },
+      update:{
+        id:25
+      }
+    }
+});
+
+
+deep.refresh(view, {} || "...")
+deep.refresh("views::/campaign/subs/info", { id:12 })
+deep.refresh("views::/campaign/subs/update")
+deep.refresh("views::/campaign", { id:45 })
+deep.refresh("views::/") // refresh all
+
+deep.show("views::...")
+deep.hide("views::...")
+
+deep.route("/campaign/12")
+deep.route({
+  campaign:{
+    id:12,
+    info:{
+      id:13
+    }
+  }
+})
+
+
+dans le template : context = 
+
+{
+  what:what,
+  params:routeParams,
+  getRoute:function(path){
+
+  }
+}
+
+example : dans infos : 
+href={% getRoute("./info/12") %}   // produit la route /campaign/13/info/12/update/13
+*/
+
 
 
