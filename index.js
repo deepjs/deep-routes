d10d611088843679458476ee5eed12c40d301fb9
Gilles Coomans <gilles.coomans@gmail.com>
2014-02-07 23:01:18 +0100

    add facilities + debug oldRoute pattern

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ define(["require", "deepjs/deep", "./lib/route", "./lib/mapper", "./lib/route-no
             msg = "RouterError : ";
         return this.Error(500, msg, report, fileName, lineNum);
     };
-
     require("./lib/route");
 	require("./lib/route-node");
     require("./lib/mapper");
@@ -33,10 +32,19 @@ define(["require", "deepjs/deep", "./lib/route", "./lib/mapper", "./lib/route-no
         if(!closure.node)
             return null;
         return closure.node.getRoute();
-    }
+    };
+
+    deep.routeMap = function(){
+        if(!closure.map)
+            return null;
+        return closure.map;
+    };
+
+    var oldRoute = null;
 
     deep.route = function(route, strict)
     {
+        // console.log("deep.route : ", route, closure.node);
         if(!route)
         {
             if(!closure.node)
@@ -55,23 +63,25 @@ define(["require", "deepjs/deep", "./lib/route", "./lib/mapper", "./lib/route-no
         else if(typeof route === 'object')
             return deep.createRouteMap(route, strict)
             .done(function(node){
+                closure.map = route;
                 closure.node = node;
                 node.emitter = emitter;
+                node.init = function(){
+                    var uri = window.location.hash.substring(1) || "/";
+                    console.log("route init : ", uri);
+                    this.route(uri);
+                };
             })
             .logError();
         else
         {
             if(!closure.node)
                 throw deep.errors.Error(500,"you need to define a root Route map before using deep.route.");
-            return deep.when(closure.node.route(route)).done(function(s){
-                if(!s)
-                    return;
-                var em = closure.node.root;
-                if(!em)
-                    em = closure.node;
-                if(em.emitter)
-                    em.emitter.emit("refreshed");
-            });
+            if(oldRoute == route)
+                 return;
+            oldRoute = route;
+            //console.log("\ndeep.route : ",route,"\n\n")
+            return deep.when(closure.node.route(route));
         }
     };
 
