78d91a5014005230aa040158392f05519ac3ee51
Gilles Coomans <gilles.coomans@gmail.com>
2014-02-21 10:51:31 +0100

    add .route chain handle, add dp-route enhancer, add core test cases + work on todo

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 /**
  * @author Gilles Coomans <gilles.coomans@gmail.com>
  */
+/*
+    feature : recode group test in route map : /[q:query,brol,!truc,(i:start/i:end)]            OK
+    
+    ROUTE DSL : add $ in end to say : finished with or finished : aka : 
+    /campaign/s:id/$     :   say : match me if there is only /campaign/s:id and nothing more        OK
+
+    add route(>info,>update)::./brol   or equivalent                    OK
+
+    so : what's next : 
+    - redirection in map? --> as it's an automate : no sens.
+            --> maybe set redirection (route transformation) in mapper after compilation.
+            they will be at higher priority
+            ex : 
+            deep.route().redirection("/campaign/s:id", "/brol/{ id }")
+
+    - staters ? will see when it's needed.
+
+    - ocm at any level : 
+        - it means that views manage ocm on what, how, where, etc
+        - or any route entry itself could be ocm
+
+    - condition in views and routing : 
+        maybe routing could handle conditions in same way than views do
+        ==> test condition before test route
+
+    - maybe add in route matcher dsl : index of matching in front of route : 
+    aka : 4/info/33
+    could be label
+ */
 "use strict";
 if (typeof define !== 'function') {
     var define = require('amdefine')(module);
 }
-define(["require", "deepjs/deep", "./lib/route", "./lib/mapper", "./lib/route-node"], function(require, deep)
+define(["require", "deepjs/deep", "./lib/route", "./lib/mapper", "./lib/route-node", "deepjs/lib/view"], function(require, deep)
 {
     deep.router = deep.router || {};
 	deep.errors.Route = function(msg, report, fileName, lineNum) {
@@ -84,6 +113,16 @@ define(["require", "deepjs/deep", "./lib/route", "./lib/mapper", "./lib/route-no
             return deep.when(closure.node.route(route));
         }
     };
+    if(!deep.isNode)
+        deep.Chain.addHandle("route", function (route) {
+            var self = this;
+            var func = function (s, e) {
+                return deep.route(route);
+            };
+            func._isDone_ = true;
+            deep.utils.addInChain.call(self, func);
+            return this;
+        });
 
     deep.route.on = function(type, callback)
     {
@@ -101,6 +140,21 @@ define(["require", "deepjs/deep", "./lib/route", "./lib/mapper", "./lib/route-no
         return closure.node.refresh();
     };
 
+    if(!deep.isNode)
+        deep.ui.View.htmlEnhancers["dp-route"] = function(node){
+            var self = this;
+            $(node).click(function(e){
+                e.preventDefault();
+                var route = self.getRoute($(node).attr("dp-route"));
+                deep.route(route);
+            });
+        };
+
+    deep.coreUnits = deep.coreUnits || [];
+    deep.coreUnits.push("js::deep-routes/units/route-dsl");
+    deep.coreUnits.push("js::deep-routes/units/map-dsl");
+
+    return deep;
 });
 
 
