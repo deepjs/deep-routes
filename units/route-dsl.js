d881e8eaa93e16d0c4f1d2c83b118a482cf7a699
Gilles Coomans <gilles.coomans@gmail.com>
2015-03-24 17:17:30 +0100

    follow deep.nodes refactoring

--- units/route-dsl.js
+++ units/route-dsl.js
@@ -12,7 +12,7 @@ define(["require", "deepjs/deep"], function(require, deep, Unit) {
 		tests: {
 			base: function() {
 				var route = new deep.Route("/campaign/?s:id/update/?q:query/?(i:start/i:end)");
-				return deep(route.match("/campaign/12/update/?foo"))
+				return deep.nodes(route.match("/campaign/12/update/?foo"))
 					.equal({
 						catched: ["campaign", "12", "update", "?foo"],
 						output: {
@@ -26,7 +26,7 @@ define(["require", "deepjs/deep"], function(require, deep, Unit) {
 			},
 			block: function() {
 				var route = new deep.Route("/campaign/?s:id/update/?q:query/?(i:start/i:end)");
-				return deep(route.match("/campaign/12/update/12/45"))
+				return deep.nodes(route.match("/campaign/12/update/12/45"))
 					.equal({
 						catched: ["campaign", "12", "update", 12, 45],
 						output: {
@@ -41,7 +41,7 @@ define(["require", "deepjs/deep"], function(require, deep, Unit) {
 			},
 			block_false: function() {
 				var route = new deep.Route("/campaign/?s:id/update/?q:query/?(i:start/i:end)");
-				return deep(route.match("/campaign/12/update/45"))
+				return deep.nodes(route.match("/campaign/12/update/45"))
 					.equal({
 						catched: ["campaign", "12", "update"],
 						output: {
@@ -54,7 +54,7 @@ define(["require", "deepjs/deep"], function(require, deep, Unit) {
 			},
 			bQuery_block: function() {
 				var route = new deep.Route("/campaign/?s:id/update/?q:query/?(i:start/i:end)");
-				return deep(route.match("/campaign/12/update/?hello/12/56"))
+				return deep.nodes(route.match("/campaign/12/update/?hello/12/56"))
 					.equal({
 						catched: ["campaign", "12", "update", "?hello", 12, 56],
 						output: {
@@ -70,23 +70,23 @@ define(["require", "deepjs/deep"], function(require, deep, Unit) {
 			},
 			notMatch: function() {
 				var route = new deep.Route("/campaign/?s:id/update/?q:query/?(i:start/i:end)");
-				return deep(route.match("/campaign/update/12/45")).equal(false);
+				return deep.nodes(route.match("/campaign/update/12/45")).equal(false);
 			},
 			not: function() {
 				var route = new deep.Route("/!campaign/?s:id/update/?q:query/?(i:start/i:end)");
-				return deep(route.match("/campaign/12")).equal(false);
+				return deep.nodes(route.match("/campaign/12")).equal(false);
 			},
 			not2: function() {
 				var route = new deep.Route("/campaign/!hello/doe");
-				return deep(route.match("/campaign/hello/doe")).equal(false);
+				return deep.nodes(route.match("/campaign/hello/doe")).equal(false);
 			},
 			not3: function() {
 				var route = new deep.Route("/campaign/!doe");
-				return deep(route.match("/campaign/doe")).equal(false);
+				return deep.nodes(route.match("/campaign/doe")).equal(false);
 			},
 			not4: function() {
 				var route = new deep.Route("/campaign/!doe");
-				return deep(route.match("/campaign/johnny"))
+				return deep.nodes(route.match("/campaign/johnny"))
 					.equal({
 						catched: ["campaign", "johnny"],
 						output: {},
@@ -97,7 +97,7 @@ define(["require", "deepjs/deep"], function(require, deep, Unit) {
 			},
 			disjonction: function() {
 				var route = new deep.Route("/[campaign,doe]/s:id");
-				return deep(route.match("/doe/45"))
+				return deep.nodes(route.match("/doe/45"))
 					.equal({
 						catched: ["doe", "45"],
 						output: {
@@ -110,12 +110,12 @@ define(["require", "deepjs/deep"], function(require, deep, Unit) {
 			},
 			disjonction_block_false: function() {
 				var route = new deep.Route("/[campaign,(doe/brol)]/s:id");
-				return deep(route.match("/doe/45"))
+				return deep.nodes(route.match("/doe/45"))
 					.equal(false);
 			},
 			disjonction_block: function() {
 				var route = new deep.Route("/[campaign,(doe/brol)]/s:id");
-				return deep(route.match("/doe/brol/45"))
+				return deep.nodes(route.match("/doe/brol/45"))
 					.equal({
 						catched: ["doe", "brol", "45"],
 						output: {
@@ -128,7 +128,7 @@ define(["require", "deepjs/deep"], function(require, deep, Unit) {
 			},
 			disjonction_int: function() {
 				var route = new deep.Route("/[i:test,(doe/brol)]/s:id");
-				return deep(route.match("/13/45"))
+				return deep.nodes(route.match("/13/45"))
 					.equal({
 						catched: [13, "45"],
 						output: {
@@ -142,11 +142,11 @@ define(["require", "deepjs/deep"], function(require, deep, Unit) {
 			},
 			endFalse: function() {
 				var route = new deep.Route("/campaign/$");
-				return deep(route.match("/campaign/12")).equal(false);
+				return deep.nodes(route.match("/campaign/12")).equal(false);
 			},
 			endOk: function() {
 				var route = new deep.Route("/campaign/$");
-				return deep(route.match("/campaign"))
+				return deep.nodes(route.match("/campaign"))
 					.equal({
 						catched: ["campaign"],
 						output: {},
@@ -157,7 +157,7 @@ define(["require", "deepjs/deep"], function(require, deep, Unit) {
 			},
 			catcheEnd: function() {
 				var route = new deep.Route("/campaign/p:path");
-				return deep(route.match("/campaign/this/is/your/path"))
+				return deep.nodes(route.match("/campaign/this/is/your/path"))
 				.equal({
 					"catched": ["campaign", "this", "is", "your", "path"],
 					"output": {
